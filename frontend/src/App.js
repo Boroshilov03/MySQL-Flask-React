1cca8a59eb35cbeada4b8d735e2cd5618f47da92
Mirlan Boroshilov <mika.borosh@gmail.com>
2024-02-24 21:25:54 -0800

    Connecting to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    effect
+    return () => {
+      cleanup
+    };
+  }, [input]);
+  return (
+    <div className="App">
+      <p className="text-lg text-orange-600">Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
